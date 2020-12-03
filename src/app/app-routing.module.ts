d7abaabe04cceeb508ce52342eba48a075d8354e
GabrielaGaspar <72529661+GabiGaspar@users.noreply.github.com>
2020-12-03 17:37:12 -0300

    adding categories module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //quando alguém acessar o /categories eu quero clicar no path:categories e  carregará o categories module. Após isso, no categories module eu configuro as rotas.
+  {path:'categories', loadChildren:() => import(`./pages/categories/categories.module`).then(module => module.CategoriesModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
